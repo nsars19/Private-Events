ed1c65c6d0fcd5c616e7a3a6126db3dcbc863bd6
Nick <nsardonini@gmail.com>
2020-07-21 17:00:06 -0400

    add partial for page headings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    return unless session[:current_user_id]
+    @current_user = User.find(session[:current_user_id])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   has_many :event_attendees, foreign_key: :event_attendee_id
   has_many :attended_events, through: :event_attendees
   has_many :created_events, foreign_key: :creator_id, class_name: "Event"
+
+  def upcoming_events
+    self.attended_events
+  end
+
+  def previous_events
+  end
 end
