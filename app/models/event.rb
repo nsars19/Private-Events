9df589f09acb02a4b591441eac22e493ac544d94
Nick <nsardonini@gmail.com>
2020-07-25 11:42:59 -0400

    change class methods to scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,13 +3,19 @@ class Event < ApplicationRecord
   has_many :attendees, through: :event_attendees, source: :event_attendee
   belongs_to :creator, class_name: "User"
 
-  def self.past
-    @past = Event.where("event_date < :time_now",
-      {time_now: Time.now})
-  end
+  scope :past, -> { where("event_date < :time_now",
+    {time_now: Time.now}) }
 
-  def self.upcoming
-    @upcoming = Event.where("event_date > :time_now",
-      {time_now: Time.now})
-  end
+  scope :upcoming, -> { where("event_date > :time_now",
+    {time_now: Time.now}) }
+
+  # def self.past
+  #   @past = Event.where("event_date < :time_now",
+  #     {time_now: Time.now})
+  # end
+
+  # def self.upcoming
+  #   @upcoming = Event.where("event_date > :time_now",
+  #     {time_now: Time.now})
+  # end
 end
